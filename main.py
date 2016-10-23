b65f20e400933020358b37e17e387d6958fb1ed4
TakuroFukamizu <takuro.f.1987@gmail.com>
2016-10-23 22:49:16 +0900

    add startup status lde

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import atexit
 import json
 import time
 from datetime import datetime
@@ -17,8 +18,9 @@ import RPi.GPIO as GPIO
 }
 """
 
-PIN_SWITCH = 4
-PIN_BELL = 17
+PIN_SWITCH  = 4  #GPIO  4 : PIN  7
+PIN_BELL    = 17 #GPIO 17 : PIN 11
+PIN_RUNNING = 26 #GPIO 26 : PIN 37
 
 def load_properties():
     basedir = os.path.dirname(os.path.abspath(__file__))
@@ -32,19 +34,31 @@ def create_knock_message():
     attime = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
     return msg_template.format(time=attime)
 
+def goodbye():
+    # clean up GPIO
+    GPIO.output(PIN_BELL   , False)
+    GPIO.output(PIN_RUNNING, False)
+    GPIO.cleanup()
 
 if __name__ == "__main__":
+    atexit.register(goodbye) # Exit handler
+
+    # setup GPIO
     GPIO.setwarnings(False)
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PIN_SWITCH , GPIO.IN)
     GPIO.setup(PIN_BELL   , GPIO.OUT)
+    GPIO.setup(PIN_RUNNING, GPIO.OUT)
 
-    GPIO.output(PIN_BELL, False)
+    GPIO.output(PIN_BELL   , False)
+    GPIO.output(PIN_RUNNING, False)
 
     # read private config
     properties = load_properties()
     api_token = properties['line_api_token']
+
+    GPIO.output(PIN_RUNNING, True)
 
     while True:
         # fetch swtich status -- 5 times/1 sec
