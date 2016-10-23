a88568118838ab2538957f6674c8e1d35a968bac
TakuroFukamizu <takuro.f.1987@gmail.com>
2016-10-23 19:20:37 +0900

    separate user setting to external json file

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 from LineNotifyRequest import *
 
-api_token = 'YOUR_TOKEN';
+"""
+# properties.json template
+{
+    "line_api_token" : "YOUR_TOKEN"
+}
+"""
+
+def load_properties():
+    pf = open('properties.json', 'r')
+    properties = json.load(pf)
+    f.close()
+    return properties
 
 if __name__ == "__main__":
+    # read private config
+    properties = load_properties()
+    api_token = properties['line_api_token']
+
+    # send Line notify
     req = LineNotifyRequest()
     req.setToken(api_token)
     req.setMessage('hogehoge')
